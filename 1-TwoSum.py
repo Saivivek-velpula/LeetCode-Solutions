886d214e47da3c2bfb7149ea5245ae7fabbccc58
Saivivek-velpula <vvelpulavenkatasai@gmail.com>
2025-09-05 16:23:26 +0530

    Update 1-TwoSum.py

--- 1-TwoSum.py
+++ 1-TwoSum.py
@@ -1,3 +1,17 @@
+"""
+Problem: 1. Two Sum
+Link: https://leetcode.com/problems/two-sum/
+Level: Easy
+
+Description:
+Given an array of integers nums and an integer target, return indices of the two numbers 
+such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+"""
 class Solution(object):
     def twoSum(self, nums, target):
         """
